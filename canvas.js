b03cad8a5a3b980147647f9a9b2f1c4e9f5de467
Brenorp <brenorpedrosa@gmail.com>
2022-09-21 18:14:35 -0300

    Refactor: code cleanup
    Refactored code to comply with functional scope programming

--- canvas.js
+++ canvas.js
@@ -1,63 +1,87 @@
 
-var canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var context = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 
-var arraySize = 25;
-var pointer = 0;
-var temp 
-var source = animate;
+const arraySize = 25;
+let pointer = 0;
 
-createArray(arraySize);
+let rectArray =[];
+rectArray = createArray(arraySize);
+
+
+let source = animate;
 requestAnimationFrame(source);
 
 function animate() {
-     //start of draw frame block
-    var x = 100; //x axis window position
+    
+    drawSorting(pointer, iterator); 
+
+
+    bubbleSort();
+    [pointer, iterator] = bubbleSortIterator(pointer, iterator);
+
+
+    if (iterator == -1) { //reset array + delay call
+        drawSorted();
+        
+        rectArray = createArray(25);//reset array
+
+        source = delay; //animation delay function
+    }
+
+    requestAnimationFrame(source);
+}
+
+
+
+
+
+function drawSorted() {
+    let x = 100;
+    for (var i = 0; i < rectArray.length; i++) { //draw finished array
+        context.fillStyle = 'rgba(0, 255, 0, 1)';
+        context.fillRect(x, 300, 10, (rectArray[i]) * -1);
+        x = x + 12;
+    }
+}
+
+function drawSorting() {
+    let x = 100; //x axis window position
     context.clearRect(0, 0, innerWidth, innerHeight);
     for (var i = 0; i < rectArray.length; i++) {
-        if (i == pointer) {
+        if (i == pointer) { 
             context.fillStyle = 'rgba(0, 0, 255, 0.75)';
-        } else if (i <= iterator+1) {
+        } else if (i <= iterator + 1) {
             context.fillStyle = 'rgba(0, 255, 0, 0.25)';
-        }else{
+        } else {
             context.fillStyle = 'rgba(0, 255, 0, 1)';
         }
         context.fillRect(x, 300, 10, (rectArray[i]) * -1);
         x = x + 12;
     } //end of draw frame block
 
+}
 
-    if (rectArray[pointer] > rectArray[pointer + 1]) {//bubblesort algorithm
-        temp = rectArray[pointer + 1];
+function bubbleSortIterator() {
+    if (pointer < iterator) { //bubblesort iteration
+        pointer++;
+        return [pointer, iterator];
+    }
+        iterator--;
+        pointer = 0;
+        return [pointer, iterator];
+    
+}
+
+function bubbleSort() {
+    if (rectArray[pointer] > rectArray[pointer + 1]) { //bubblesort algorithm
+        const temp = rectArray[pointer + 1];
         rectArray[pointer + 1] = rectArray[pointer];
         rectArray[pointer] = temp;
     }
-
-    if (pointer < iterator) {//bubblesort iteration
-        pointer++;
-    } else {
-        iterator--;
-        pointer = 0;
-    }
-
-
-    if (iterator == -1) { //reset array + delay call
-        x = 100;
-        for (var i = 0; i < rectArray.length; i++) {//draw finished array
-            context.fillStyle = 'rgba(0, 255, 0, 1)';
-            context.fillRect(x, 300, 10, (rectArray[i]) * -1);
-            x = x + 12; 
-        }
-
-        createArray(25);//reset array
-
-        source = delay; //animation delay function
-    }
-
-    requestAnimationFrame(source);
 }
 
 function delay() {
@@ -70,11 +94,12 @@ function delay() {
 }
 
 function createArray(arraySize) {
-    rectArray = [];
-    for (var i = 0; i < arraySize; i++) {
-        rectArray.push(parseInt(Math.random() * 100, 10));
-    }
-    iterator = rectArray.length - 2;
+    let array = [];
+    array = Array(arraySize).fill(0);
+
+    iterator = array.length - 2;
+
+    return array.map(()=>parseInt(Math.random() * 100, 10));
 
 }
 
